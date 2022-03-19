d3188c2b98a8ff7bfd3d327a8315e05c9fc7d541
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-19 20:48:26 -0300

    Links component

---
+++ src/components/Links/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Container } from '../themes/global';
+import { LinkTag, Link } from './styles';
+
+
+export default function Links(){
+  return(
+    <Container>
+      <LinkTag>
+        <Link href="https://github.com/gabrielcaamargo" target="_blank"><FontAwesomeIcon icon={faGithub}/></Link>
+        <Link href="https://instagram.com/gabcamargo" target="_blank"><FontAwesomeIcon icon={faInstagram}/></Link>
+        <Link href="https://linkedin.com/in/gabrielcaamargo/" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></Link>
+      </LinkTag>
+    </Container>
+  )
+}
---
+++ src/components/Links/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const LinkTag = styled.div`
+  display: flex;
+  font-size: 28px;
+`
+
+export const Link = styled.a`
+  margin-right: 5px;
+  color: #4F6272;
+  padding: 10px 0;
+`
