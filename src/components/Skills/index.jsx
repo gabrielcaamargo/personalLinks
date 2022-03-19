c67661a7587a40c8aebaa96446429976a9eab54d
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-19 20:58:56 -0300

    renaming stacks to skills

---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { faReact, faJs } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { IconsContainer, Title } from './styles';
+import { Container } from '../../components/themes/global' 
+
+export default function Stacks(){
+  return(
+    <Container>
+      <IconsContainer>
+        <Title>Skills</Title> 
+        <FontAwesomeIcon icon={faReact} />
+        <FontAwesomeIcon icon={faJs} />
+    </IconsContainer>
+    </Container>
+  )
+}
---
+++ src/components/Skills/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Title = styled.h2`
+  font-size: 26px;  
+  text-align: center;
+  font-weight: bolder;
+  color: #3A4754;
+`
+export const IconsContainer = styled.div`
+  text-align: center;
+  font-size: 30px;
+  color: #4F6272;
+  padding: 0px 8px;
+  
+  & .fa-react{
+    margin-right: 5px;
+  }
+`
