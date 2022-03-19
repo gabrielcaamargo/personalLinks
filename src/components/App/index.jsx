213e9c1c992731eeb8edae1109ea5d7ed120c913
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-19 18:05:45 -0300

    setting styled-components links && creating components folder

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GlobalStyle from '../themes/global'
+
+export default function App(){
+  return(
+    <>
+      <GlobalStyle />
+      <h1>Olá</h1>
+    </>
+  )
+}
---
+++ src/components/themes/global.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  body {
+    background: #404E5C;
+    color: #DE9E36;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+`;
