2c4a42e141197e9bed9db8878645f3e932dde820
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-19 20:14:47 -0300

    adjusting stacks width

--- src/components/Stacks/styles.js
+++ src/components/Stacks/styles.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 25vw;
   background-color: #B7C3F3;
   border-radius: 15px;
-  padding: 8px 16px;
+  padding: 2px 7.5rem;
   margin-top: 15px;
   display: flex;
   flex-direction: column;
+  text-align: center;
 `
 
 export const Title = styled.h2`
@@ -20,6 +20,7 @@ export const IconsContainer = styled.div`
   text-align: center;
   font-size: 30px;
   color: #4F6272;
+  padding: 0px 8px;
   
   & .fa-react{
     margin-right: 5px;
